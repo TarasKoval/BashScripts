663cfa5b7388597831bcb51ac1e19b36f8a9ee05
TarasKoval <antares13.1998@gmail.com>
2018-03-11 14:24:38 +0200

    tests0.1

---
+++ tests/collatz_conjecture_test.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bats
+
+@test "zero steps for one" {
+  #
+  run bash collatz.sh 1
+
+  [ "$status" -eq 0 ]
+  [ "$output" -eq 0 ]
+}
+
+@test "divide if even" {
+  
+  run bash collatz.sh 16
+
+  [ "$status" -eq 0 ]
+  [ "$output" -eq 4 ]
+}
+
+@test "even and odd steps" {
+  
+  run bash collatz.sh 12
+
+  [ "$status" -eq 0 ]
+  [ "$output" -eq 9 ]
+}
+
+@test "large number of even and odd steps" {
+  
+  run bash collatz.sh 1000000
+
+  [ "$status" -eq 0 ]
+  [ "$output" -eq 152 ]
+}
+
+@test "zero is an error" {
+  
+  run bash collatz.sh 0
+
+  [ "$status" -eq 1 ]
+  [ "$output" == "Error: Only positive numbers are allowed" ]
+}
+
+@test "negative value is an error" {
+  
+  run bash collatz.sh -15
+
+  [ "$status" -eq 1 ]
+  [ "$output" == "Error: Only positive numbers are allowed" ]
+}
